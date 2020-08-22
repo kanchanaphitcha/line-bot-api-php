dd9e507ccbd2302c17e0261390570478847d56f6
kanchanaphitcha <70052241+kanchanaphitcha@users.noreply.github.com>
2020-08-22 16:14:41 +0700

    Update line.php

--- line.php
+++ line.php
@@ -15,9 +15,7 @@ if ( sizeof($request_array['events']) > 0 )
   $reply_message = '';
   $reply_token = $event['replyToken'];
 
-  if ( $event['type'] == 'message' ) 
-  {
-   if($text == "ชื่ออะไร" || $text == "ชื่ออะไรคะ" || $text == "ชื่ออะไรครับ" || $text == "ชื่อ" || $text == "ชื่อไร"){
+ if($text == "ชื่ออะไร" || $text == "ชื่ออะไรคะ" || $text == "ชื่ออะไรครับ" || $text == "ชื่อ" || $text == "ชื่อไร"){
    $reply_message = 'ชื่อของฉัน คือ Hathaikan';
   }
      if($text == "สถานการณ์โควิดวันนี้" || $text == "covid19" || $text == "covid-19" || $text == "Covid-19"){
@@ -37,50 +35,3 @@ if ( sizeof($request_array['events']) > 0 )
       $reply_message = 'ติดเชื้อสะสม '. $obj->{'Confirmed'} . 'คน';
    $reply_message += '<br>\r\n รักษาหายแล้ว '. $obj->{'Recovered'} . 'คน';
     }
-   
-		   
-  
-  //$reply_message = '('.$text.') ได้รับข้อความเรียบร้อย!!';	
-	   
-		
-		
-   }
-   else
-    $reply_message = 'ระบบได้รับ '.ucfirst($event['message']['type']).' ของคุณแล้ว';
-  
-  }
-  else
-   $reply_message = 'ระบบได้รับ Event '.ucfirst($event['type']).' ของคุณแล้ว';
- 
-  if( strlen($reply_message) > 0 )
-  {
-   //$reply_message = iconv("tis-620","utf-8",$reply_message);
-   $data = [
-    'replyToken' => $reply_token,
-    'messages' => [['type' => 'text', 'text' => $reply_message]]
-   ];
-   $post_body = json_encode($data, JSON_UNESCAPED_UNICODE);
-
-   $send_result = send_reply_message($API_URL, $POST_HEADER, $post_body);
-   echo "Result: ".$send_result."\r\n";
-  }
- }
-}
-
-echo "OK";
-
-function send_reply_message($url, $post_header, $post_body)
-{
- $ch = curl_init($url);
- curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
- curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
- curl_setopt($ch, CURLOPT_HTTPHEADER, $post_header);
- curl_setopt($ch, CURLOPT_POSTFIELDS, $post_body);
- curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
- $result = curl_exec($ch);
- curl_close($ch);
-
- return $result;
-}
-
-?>
