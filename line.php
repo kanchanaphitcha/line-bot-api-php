fbd3ca057b449086c4ba3f6b35ea8d71459ac016
kanchanaphitcha <70052241+kanchanaphitcha@users.noreply.github.com>
2020-08-22 15:57:42 +0700

    Update line.php

--- line.php
+++ line.php
@@ -20,9 +20,10 @@ if ( sizeof($request_array['events']) > 0 )
    
    if( $event['message']['type'] == 'text' )
    {
-	if($test == "ชื่ออะไร" || $text == "ชื่ออะไรคะ" || $text == "ชื่ออะไรครับ" || $text == "ชื่อ" || $text == "ชื่อไร"){
-        $reply_message = 'ชื่อของฉัน คือ kanchanaphitcha';
+	f($test == "ชื่ออะไร" || $text == "ชื่ออะไรคะ" || $text == "ชื่ออะไรครับ" || $text == "ชื่อ" || $text == "ชื่อไร"){
+   $reply_message = 'ชื่อของฉัน คือ kanchanaphitcha';
   }
+  
   //$reply_message = '('.$text.') ได้รับข้อความเรียบร้อย!!';	
 	   
 		
