362f7917e6d5f8c75e2b4bdc9dfa5a94e4cbef3a
kanchanaphitcha <70052241+kanchanaphitcha@users.noreply.github.com>
2020-08-22 23:21:02 +0700

    Update line.php

--- line.php
+++ line.php
@@ -26,6 +26,10 @@ if ( sizeof($request_array['events']) > 0 )
    $reply_message = 'ชื่อของฉัน คือ ME';
   }
     
+    if($text == "ชื่อผู้พัฒา"){
+   $reply_message = 'นางสาวกาญจนพิชชา มีสุข'.'\n'.$reply_message = 'รหัสนิสิต 61160276';
+  }
+    
      if($text == "สถานการณ์โควิดวันนี้" || $text == "covid19" || $text == "covid-19" || $text == "Covid-19"){
      $url = 'https://covid19.th-stat.com/api/open/today';
      $ch = curl_init($url);
