6844c02fd5a317e845bd9add05d1586a759ea186
kanchanaphitcha <70052241+kanchanaphitcha@users.noreply.github.com>
2020-08-22 23:34:04 +0700

    Update line.php

--- line.php
+++ line.php
@@ -44,7 +44,7 @@ if ( sizeof($request_array['events']) > 0 )
      $obj = json_decode($result);
      
      $reply_message = $result;
-     $reply_message = 'ติดเชื้อสะสม '. $obj->{'Confirmed'}.'คน ' "\n".$reply_message = 'รักษาหายแล้ว' . $obj->{'Recovered'}.'คน';
+     $reply_message = 'ติดเชื้อสะสม '. $obj->{'Confirmed'}.'คน ' . "\n".$reply_message = 'รักษาหายแล้ว' . $obj->{'Recovered'}.'คน';
  //$reply_message +='\r\n รักษาหาายแล้ว '. $obj->{'NewConfirmed'}.'คน';
     }
   //$reply_message = '('.$text.') ได้รับข้อความเรียบร้อย!!';   
