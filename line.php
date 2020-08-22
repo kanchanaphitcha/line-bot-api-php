4f00213fce4a8175944777f8f20ccc54115ba3c2
kanchanaphitcha <70052241+kanchanaphitcha@users.noreply.github.com>
2020-08-22 23:07:35 +0700

    Update line.php

--- line.php
+++ line.php
@@ -21,9 +21,13 @@ if ( sizeof($request_array['events']) > 0 )
    if( $event['message']['type'] == 'text' )
    {
   $text = $event['message']['text'];
+    if($text == "ชื่อผู้พัฒนา") {
+        $reply_message = 'นางสาวกาญจนพิชชา มีสุข'."\n".$reply_message = "รหัสนิสิต 61160276";
+    
+    }
   
     if($text == "ชื่ออะไร" || $text == "ชื่ออะไรคะ" || $text == "ชื่ออะไรครับ" || $text == "ชื่อ" || $text == "ชื่อไร"){
-   $reply_message = 'ชื่อของฉัน คือ BOTME';
+   $reply_message = 'ชื่อของฉัน คือ kanchanaphitcha-276';
   }
     if($text == "สถานการณ์โควิดวันนี้" || $text == "covid19" || $text == "covid-19" || $text == "Covid-19"){
      $url = 'https://covid19.th-stat.com/api/open/today';
@@ -39,7 +43,7 @@ if ( sizeof($request_array['events']) > 0 )
      $obj = json_decode($result);
      
      $reply_message = $result;
-     $reply_message = 'ติดเชื้อสะสม '. $obj->{'Confirmed'}.'คน \r\n รักษาหาายแล้ว '. $obj->{'NewConfirmed'}.'คน';
+     $reply_message = 'ติดเชื้อสะสม '. $obj->{'Confirmed'}.'คน '."\n". $reply_message = "รักษาหายแล้ว".$obj->{'Recovered'}. 'คน',
  //$reply_message +='\r\n รักษาหาายแล้ว '. $obj->{'NewConfirmed'}.'คน';
     }
   //$reply_message = '('.$text.') ได้รับข้อความเรียบร้อย!!';   
